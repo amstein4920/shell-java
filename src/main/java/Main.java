0e047c565520bff5ada6cbb6bece82793883c5da
Rags <amstein4920@gmail.com>
2025-11-14 15:39:45 +0800

    Include exit codes and rework input parsing for usability

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,22 +1,32 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
+        int exitCode = 0;
 
         repl: do {
-        System.out.print("$ ");
+            System.out.print("$ ");
 
-        String command = scanner.nextLine();
+            // String command = scanner.next();
+            // String arguments = scanner.nextLine();
 
-        switch (command.toUpperCase()) {
-            case "EXIT":
-                break repl;
-            default:
-                System.out.println(command + ": command not found");
-        }
-    } while (true);
+            String input = scanner.nextLine();
+            String[] parts = input.split(" ");
+            String command = parts[0];
+            String[] arguments = Arrays.copyOfRange(parts, 1, parts.length);
 
+
+            switch (command.toUpperCase()) {
+                case "EXIT":
+                    exitCode = Integer.parseInt(arguments[0].trim());
+                    break repl;
+                default:
+                    System.out.println(command + ": command not found");
+            }
+        } while (true);
         scanner.close();
+        System.exit(exitCode);
     }
 }
