32917c4a41b8c35c5e2615f033ef7828b40278b7
Rags <amstein4920@gmail.com>
2025-11-14 16:14:31 +0800

    Include type builtin

---
+++ src/main/java/Builtin.java
@@ -0,0 +1,15 @@
+    enum Builtin {
+            EXIT,
+            ECHO,
+            TYPE;
+
+
+            public static Boolean isBuiltin(String input) {
+                for (Builtin builtin : Builtin.values()) {
+                    if (builtin.name().equals(input.toUpperCase())) {
+                        return true;
+                    }
+                }
+                return false;
+            }
+        }
