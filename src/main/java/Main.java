266a651ce2d792ceea7bd5f58e432500c180a795
Rags <amstein4920@gmail.com>
2025-11-14 15:19:44 +0800

    Return invalid commands

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,5 +3,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         System.out.print("$ ");
+        Scanner scanner = new Scanner(System.in);
+
+        String command = scanner.nextLine();
+
+        switch (command) {
+            default:
+                System.out.println(command + ": command not found");
+        }
+
+        scanner.close();
     }
 }
