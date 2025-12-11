f5da5b0353572858219ab17e5850f59f1161dcb4
Rags <amstein4920@gmail.com>
2025-12-12 06:12:59 +0800

    Added appending redirect for stdout

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,6 @@
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.PrintStream;
 import java.util.Arrays;
 import java.util.Scanner;
@@ -19,30 +21,7 @@ public class Main {
             Parser parser = new Parser();
             String[] arguments = parser.parse(input); // [command, args?]
             PrintStream console = System.out;
-
-            for (int i = 0; i < arguments.length; i++) {
-                if (i < arguments.length - 1) {
-                    if (Arrays.asList(new String[] { ">", "1>" }).contains(arguments[i])) {
-                        PrintStream newPS;
-                        try {
-                            newPS = new PrintStream(arguments[i + 1]);
-                            System.setOut(newPS);
-                            arguments = Arrays.copyOfRange(arguments, 0, i);
-                        } catch (FileNotFoundException e) {
-                            System.err.println("Output File Couldn't Be Created: " + e.getMessage());
-                        }
-                    } else if (Arrays.asList(new String[] { "2>" }).contains(arguments[i])) {
-                        PrintStream newPS;
-                        try {
-                            newPS = new PrintStream(arguments[i + 1]);
-                            System.setErr(newPS);
-                            arguments = Arrays.copyOfRange(arguments, 0, i);
-                        } catch (FileNotFoundException e) {
-                            System.err.println("Error File Couldn't Be Created: " + e.getMessage());
-                        }
-                    }
-                }
-            }
+            arguments = redirectOutputs(arguments);
             Command command = Commands.get(arguments[0]);
             if (command != null) {
                 command.execute(arguments);
@@ -53,4 +32,39 @@ public class Main {
         } while (true);
     }
 
+    private static String[] redirectOutputs(String[] arguments) {
+        for (int i = 0; i < arguments.length; i++) {
+            if (i < arguments.length - 1) {
+                if (Arrays.asList(new String[] { ">", "1>" }).contains(arguments[i])) {
+                    PrintStream newPS;
+                    try {
+                        newPS = new PrintStream(arguments[i + 1]);
+                        System.setOut(newPS);
+                        arguments = Arrays.copyOfRange(arguments, 0, i);
+                    } catch (FileNotFoundException e) {
+                    }
+                } else if (Arrays.asList(new String[] { ">>", "1>>" }).contains(arguments[i])) {
+                    FileOutputStream fos;
+                    try {
+                        fos = new FileOutputStream(new File(arguments[i + 1]), true);
+                        PrintStream newPS = new PrintStream(fos);
+                        System.setOut(newPS);
+                        arguments = Arrays.copyOfRange(arguments, 0, i);
+                    } catch (FileNotFoundException e) {
+                        System.err.println("Output File Couldn't Be Created: " + e.getMessage());
+                    }
+                } else if ("2>".equals(arguments[i])) {
+                    PrintStream newPS;
+                    try {
+                        newPS = new PrintStream(arguments[i + 1]);
+                        System.setErr(newPS);
+                        arguments = Arrays.copyOfRange(arguments, 0, i);
+                    } catch (FileNotFoundException e) {
+                        System.err.println("Error File Couldn't Be Created: " + e.getMessage());
+                    }
+                }
+            }
+        }
+        return arguments;
+    }
 }
